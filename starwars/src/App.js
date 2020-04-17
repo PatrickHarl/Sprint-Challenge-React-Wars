6f1bc00acc04631ef2c102088feadfb1b34f0cc0
Patrick Harl <therandomwoodworker@gmail.com>
2020-04-17 10:22:53 -0700

    completed project + stretch

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -2,6 +2,7 @@ import React, { useState , useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 import Character from './components/Character'
+import styled from 'styled-components'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -12,20 +13,32 @@ const App = () => {
   // sync up with, if any.
 
   const url = 'https://swapi.py4e.com/api/people'
-  const [characterData, setCharacterData] = useState([])
+  const [characterData, setCharacterData] = useState({})
+  const [currentURL, setCurrentURL] = useState(url)
   
 
+  const Buttons = styled.button`
+  
+  
+    width:80px;
+    height:25px;
+    background-color:blanchedalmond;
+    border-color:blanchedalmond;
+    border-radius:5px;
+    margin: 5px;
 
+  
+  
+  `
   
 
 
  useEffect(() => {
 
-  axios.get(url)
+  axios.get(currentURL)
         .then(res => {
 
-          setCharacterData(res.data.results)
-          
+          setCharacterData(res.data)
           
 
         })
@@ -38,20 +51,30 @@ const App = () => {
         })
     
 
-  }, [])
+  }, [currentURL])
 
-  
+  if (!characterData.results)
+  {
+
+    return <h3>Loading...</h3>
+
+  }
+  else {
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
       {
 
-      characterData.map((char) => {return <Character char={char} />})
+      characterData.results.map((char) => {return <Character char={char} />})
       
       }
+      <Buttons onClick={() => {characterData.previous ? setCurrentURL(characterData.previous) : setCurrentURL(url)}}>Previous</Buttons>
+      <Buttons onClick={() => {characterData.next ? setCurrentURL(characterData.next) : setCurrentURL(url)}}>Next</Buttons>
+
     </div>
   );
+    }
 }
 
 export default App;
